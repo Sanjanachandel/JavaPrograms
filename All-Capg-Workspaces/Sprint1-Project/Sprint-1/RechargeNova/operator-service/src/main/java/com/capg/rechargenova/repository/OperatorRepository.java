8f0156565ea58e37cbcf6b0e73da5123fdb3d854
Sanjanachandel <sanjanachandel82@gmail.com>
2026-04-22 09:26:38 +0530

    updated backend

--- All-Capg-Workspaces/Sprint1-Project/Sprint-1/RechargeNova/operator-service/src/main/java/com/capg/rechargenova/repository/OperatorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capg.rechargenova.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.rechargenova.entity.Operator;
-
-public interface OperatorRepository extends JpaRepository<Operator, Long> {
-}
--- All-Capg-Workspaces/Sprint1-Project/Sprint-1/RechargeNova/recharge-service/src/main/java/com/capg/rechargenova/exception/InvalidRechargeException.java
+++
@@ -1,7 +0,0 @@
-package com.capg.rechargenova.exception;
-
-public class InvalidRechargeException extends RuntimeException {
-    public InvalidRechargeException(String message) {
-        super(message);
-    }
-}
